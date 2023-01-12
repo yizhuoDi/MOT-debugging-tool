e4a00835391c27e2ac326a00601f0ffccbefdc0e
yizhuoDi <119659063+yizhuoDi@users.noreply.github.com>
2023-01-12 10:42:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,33 +50,19 @@ def create_fig():
     return px.imshow(io.imread(filelist[GLOBAL_IDX]), binary_backend="jpg")
 
 
-video_nums = ['MOT17-02-SDP',
-            'MOT17-04-SDP',
-            'MOT17-05-SDP',
-            'MOT17-09-SDP',
-            'MOT17-10-SDP',
-            'MOT17-11-SDP',
-            'MOT17-13-SDP']
+	seq_nums =  ['video_0',
+            'video_1',
+            'video_2',
+            'video_3',
+            'video_4',
+            'video_5',
+            'video_6',
+		    'video_7']
 
 bug_nums = ['Id-switch',
             'False positive',
             'Miss']
 
-
-"""
-                dcc.Dropdown(
-                    id="bug-type",
-                    options=[
-                        {"label": seq, "value": seq}
-                        for seq in bug_nums
-                    ],
-                    value="bug",
-                    style={'width': "50%"},
-                    clearable=False,
-                    searchable=False,
-                    className="dropdown",
-                ),
-"""
 
 # Cards
 image_annotation_card = dbc.Card(
@@ -165,84 +151,6 @@ app.layout = html.Div(
     ]
 )
 
-
-"""
-app.layout = html.Div(children=[
-    html.H1("Mot debugging", style={'text-align': 'center'}),
-    html.Div(children=[
-        html.Div(
-            children=[
-                html.Div(children="Video", className="menu-title"),
-                dcc.Dropdown(
-                    id="region-filter",
-                    options=[
-                        {"label": seq, "value": seq}
-                        for seq in video_nums
-                    ],
-                    value="Albany",
-                    style={'width': "50%"},
-                    clearable=False,
-                    className="dropdown",
-                ),
-            ]
-        ),
-        html.Div(
-            children=[
-                html.Div(children="Debugging Type", className="menu-title"),
-                dcc.Dropdown(
-                    id="type-filter",
-                    options=[
-                        {"label": seq, "value": seq}
-                        for seq in bug_nums
-                    ],
-                    value="organic",
-                    style={'width': "50%"},
-                    clearable=False,
-                    searchable=False,
-                    className="dropdown",
-                ),
-            ],
-        ),
-    ],
-        className="menu",
-    ),
-
-    html.Video(
-        controls=True,
-        id='movie_player',
-        #src = "/home/ubuntu/exp/MOTChallengeEvalKit-master/res/KITTI-17/KITTI-17_test1.mp4",
-        #src="https://www.w3schools.com/html/mov_bbb.mp4",
-        src = "https://drive.google.com/file/d/1XeMmG3oEy7AK1jqruV_Zmxs57cYsZ6h2/view?usp=share_link",
-        autoPlay=True,
-        style={'width': "50%"}
-    ),
-    dcc.Graph(
-
-        id="graph",
-        figure=example_img,
-    ),
-    dcc.Input(
-            id='bug-id',
-            value='1',
-            type='text'
-            ),
-    dcc.Slider(
-               min=0,
-               max=9,
-               marks={i: 'level{}'.format(i) for i in range(10)},
-               value=5,
-           ),
-    dbc.ButtonGroup(
-                    [
-                        dbc.Button(children="Previous", id="previous", outline=True),
-                        dbc.Button(children="Next", id="next", outline=True),
-                    ],
-                    size="lg",
-                    style={"width": "100%"},
-    ),
-])
-"""
-
 @app.callback(
     [Output("graph","figure"), Output("next", "children"), Output("previous", "children"),Output("bug_next", "n_clicks"), Output("bug_previous", "n_clicks"),Output("figure-index", "children")],
     [Input("previous", "n_clicks"), Input("next", "n_clicks"),Input("bug_previous", "n_clicks"), Input("bug_next", "n_clicks"),Input('video-seq','value'),Input('video_jump', 'n_clicks'), Input('jump', 'n_clicks'), Input('jump-index', 'value')]
@@ -259,27 +167,6 @@ def control_video(previous_n_clicks, next_n_clicks, bug_previous_n_clicks, bug_n
     # global next_children, previous_children
     calllback_context = [p["prop_id"]
                          for p in dash.callback_context.triggered][0]
-    """
-    if calllback_context == "previous.n_clicks" and is_previous == False:
-        is_previous = True
-        return_previous_text = "Stop"
-        calllback_context = ""
-
-    if calllback_context == "previous.n_clicks" and is_previous == True:
-        is_previous = False
-        return_previous_text = "Previous"
-        calllback_context = ""
-
-    if calllback_context == "next.n_clicks" and is_next == False:
-        is_next = True
-        return_next_text = "Stop"
-        calllback_context = ""
-
-    if calllback_context == "next.n_clicks" and is_next == True:
-        is_next = False
-        return_next_text = "Next"
-        calllback_context = ""
-    """
     if calllback_context == "previous.n_clicks" :
         is_previous = True
         return_previous_text = "Previous"
@@ -296,27 +183,6 @@ def control_video(previous_n_clicks, next_n_clicks, bug_previous_n_clicks, bug_n
         # fig_next_thread = threading.Thread(target=fig_next).start()
         # return fig_next_thread
 
-    """
-    if calllback_context == "bug_previous.n_clicks" and is_bug_previous == False:
-        is_bug_previous = True
-        return_bug_previous_text = "Bug_Stop"
-        calllback_context = ""
-
-    if calllback_context == "bug_previous.n_clicks" and is_bug_previous == True:
-        is_bug_previous = False
-        return_bug_previous_text = "bug_Previous"
-        calllback_context = ""
-
-    if calllback_context == "bug_next.n_clicks" and is_bug_next == False:
-        is_bug_next = True
-        return_bug_next_text = "Bug_Stop"
-        calllback_context = ""
-
-    if calllback_context == "bug_next.n_clicks" and is_bug_next == True:
-        is_bug_next = False
-        return_bug_next_text = "bug_Next"
-        calllback_context = ""
-    """
     if calllback_context == "bug_previous.n_clicks" and BUG_NUM>0:
         #is_bug_previous = False
         return_bug_previous_text = "bug_Previous"
@@ -340,33 +206,7 @@ def control_video(previous_n_clicks, next_n_clicks, bug_previous_n_clicks, bug_n
         GLOBAL_IDX = int(jump_index)-1
         debug_print('GLOBAL_IDX:', GLOBAL_IDX)
         calllback_context = ""
-
-    """
-    if is_next:
-        GLOBAL_IDX = (GLOBAL_IDX + 1) % IMG_NUM
-        debug_print('GLOBAL_IDX:', GLOBAL_IDX)
-
-    if is_previous:
-        GLOBAL_IDX = ((GLOBAL_IDX - 1) % IMG_NUM + IMG_NUM) % IMG_NUM
-        debug_print('GLOBAL_IDX:', GLOBAL_IDX)
-    
-    """
-    
-    """
-    if is_bug_next and BUG_NUM>0:
-        BUG_IDX = (BUG_IDX + 1) % BUG_NUM
-        GLOBAL_IDX = Frame_bug[BUG_IDX]
-        debug_print('GLOBAL_IDX:', GLOBAL_IDX)
-        debug_print('BUG_IDX:', BUG_IDX)
-
-    if is_bug_previous and BUG_NUM>0:
-        BUG_IDX = (BUG_IDX - 1) % BUG_NUM 
-        GLOBAL_IDX = Frame_bug[BUG_IDX]
-        debug_print('GLOBAL_IDX:', GLOBAL_IDX)
-        debug_print('BUG_IDX:', BUG_IDX)
-    """
-    
-
+        
     if calllback_context == 'video_jump.n_clicks':
         BUG_IDX=-1
         GLOBAL_IDX=0
@@ -379,11 +219,8 @@ def control_video(previous_n_clicks, next_n_clicks, bug_previous_n_clicks, bug_n
         filelist.sort(key = lambda x : int(x[(len_name+1):-4]))
 
         FileName_ids=os.path.join(FileName_ids_path,f'{video_value}_ids_frame.csv')
-    #FileName_ids=os.path.join(FileName_ids_path,f'MOT17-02-SDP_ids.csv')
         ids_res=pd.read_csv(FileName_ids)
         Frame_bug=ids_res["FrameId"]
-    #Frame_bug=Frame_bug.drop_duplicates()
-    # print(filelist)
         IMG_NUM = len(filelist)
         BUG_NUM = len(Frame_bug)
        
